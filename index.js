45469ed64f6964de0e5a5605976eb6f179effcee
Hannah Maratea <maratea101@gmail.com>
2022-10-16 14:22:00 -0400

    commit tree 22034a0188eff420ba6466ab19b757dca8988777

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-
-// Write your code here
+// Create a variable called 'multiply' set to an equation that will multiply the variables num1 and num2;
+// The result of the multiplication should be to 62.
+let num1 = 31;
+let num2 = 2;
+var multiply = num1 * num2
+// Create a variable called 'random' that will generate a random integer greater than 0.
+var random = Math.floor( Math.random() * (2 - 1) + 1 );
+console.log('toast' + random)
+// var intvalue = Math.floor( Math.random() * (2 - 1) + 1 );
+// Create a variable called 'mod' set to an equation that will calculate the remainder of dividing varialbe num3 by num4;
+// The remainder should be 4.
+let num3 = 40;
+let num4 = 10;
+var mod = num3 / num4
+// Create a variable called 'max' that finds the highest number in a set;
+// The value returned should be 20. 
+// var max = 20
+var max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20);
